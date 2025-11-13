4aaa04e98aa5cb9e2b046c8ca1ce0c318ae8b275
Heng-Yi Liu <hengyl@microsoft.com>
2025-11-13 09:31:15 -0800

    fix load_data

--- scripts/load_data.sh
+++ scripts/load_data.sh
@@ -1,5 +1,11 @@
  #!/bin/sh
 
+# Check if RUN_LOAD_DATA environment variable is set to true
+if [ "$RUN_LOAD_DATA" == "false" ]; then
+    echo "Skipping data loading script. Set RUN_LOAD_DATA to 'true' to run this script."
+    exit 0
+fi
+
 . ./scripts/load_python_env.sh
 
 echo 'Running "load_data.py"'
